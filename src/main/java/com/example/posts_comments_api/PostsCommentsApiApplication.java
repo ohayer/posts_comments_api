faa02187c424e658b78a157b308dd33453e92e4a
ohayer <olik03.2003@wp.pl>
2023-07-16 20:45:41 +0200

    Build project commit

---
+++ src/main/java/com/example/posts_comments_api/PostsCommentsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.posts_comments_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostsCommentsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostsCommentsApiApplication.class, args);
+    }
+
+}
